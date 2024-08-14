9737aa05c0a0f2f018d9135edc4cca387bb097cf
MuV1N1 <marvin.kleiner1910@gmail.com>
2024-08-14 21:13:24 +0200

    s

---
+++ src/app/pages/homePage.js
@@ -0,0 +1,17 @@
+'use client'
+
+import Navbar from "./components/navbar";
+
+
+export default function() {
+    return (
+        <>
+            <Navbar />
+            <section id="one"></section>
+            <section id="two"></section>
+            <section id="one"></section>
+            <section id="two"></section>
+
+        </>
+    );
+}
