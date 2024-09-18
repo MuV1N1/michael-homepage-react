e9e3a658c9ea383ad678036710d18087069d61f1
MuV1N1 <marvin.kleiner1910@gmail.com>
2024-09-18 21:23:02 +0200

    idk

--- src/app/pages/homePage.js
+++ src/app/pages/homePage.js
@@ -12,11 +12,14 @@ export default function HomePage() {
                         <p className="text-lg sm:text-xl mb-7">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
                         <p className="text-lg sm:text-xl mb-7">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
                     </div>
-                    <div id="logo" className="w-1/4 sm:w-1/3 flex justify-center sm:justify-center mt-4 sm:mt-0">
+                    <div id="logo" className="w-1/4 sm:w-1/3 flex justify-center sm:justify-center mt-4 sm:mt-0 ml-3">
 
-                    {/*Need new Image later*/}
-                        <img src="https://media.gettyimages.com/id/2147648930/de/foto/sporty-beautiful-black-woman-in-red-sportswear.webp?s=612x612&w=gi&k=20&c=eXHjSZyyIGimRK53eZWkKveeCQGimZX_KD0KMM4zBko=" alt="test" className=" w-48 h-48 rounded-full object-cover" />
-                    </div>
+                        {/*Need new Image later*/}
+                        <img
+                            src="https://media.gettyimages.com/id/2147648930/de/foto/sporty-beautiful-black-woman-in-red-sportswear.webp?s=612x612&w=gi&k=20&c=eXHjSZyyIGimRK53eZWkKveeCQGimZX_KD0KMM4zBko="
+                            alt="test"
+                            className="w-48 h-auto rounded-full object-cover aspect-square"
+                        />                        </div>
                 </div>
             </section>
             <section className="bg-red-900">
